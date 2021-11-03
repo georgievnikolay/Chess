5b86a6dd08abddbf83121af44fa640560f0e7daa
georgievnikolay <nikolay.georgievfx@gmail.com>
2021-11-04 00:55:11 +0200

    Queen Impl

---
+++ src/game/entities/pieces/types/Queen.c
@@ -0,0 +1,13 @@
+/*      Own include     */
+#include "Queen.h"
+
+/*    System includes   */
+
+/* Third party includes */
+
+/* Own library includes */
+
+/* Forward declarations */
+
+/*        Defines       */
+
